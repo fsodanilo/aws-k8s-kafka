b756c33279b502c743e63f4f0622c4ee958e6884
Danilotve <fsodanilo@gmail.com>
2021-09-27 11:35:07 -0300

    ingest

--- kafka-strimzi-k8s-v1beta1/build-img-strimzi.sh
+++ kafka-strimzi-k8s-v1beta1/build-img-strimzi.sh
@@ -1,1 +1,9 @@
 #! /usr/bin/env bash
+
+aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 284165992806.dkr.ecr.us-east-2.amazonaws.com
+
+docker build -t igti-repository .
+
+docker tag igti-repository:latest 284165992806.dkr.ecr.us-east-2.amazonaws.com/igti-repository:latest
+
+docker push 284165992806.dkr.ecr.us-east-2.amazonaws.com/igti-repository:latest
